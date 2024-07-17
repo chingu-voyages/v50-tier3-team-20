9b75cc4685a1cd6e08e53737de2eff8bd83c1a60
subsurfmodifier <a2plewins@gmail.com>
2024-07-17 19:51:58 +0900

    font and image for the image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import './globals.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6 bg-yellow-500">
+      <div className='h-400 w-800 text-5xl text-black'>
+        Healing
+      </div>
+      <Image
+        src="/homepage.jpg"
+        width={6016}
+        height={4016}
+        alt="family photo for the homepage"
+      />
     </main>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Henny_Penny } from 'next/font/google';
+ 
+export const hennyPenny = Henny_Penny({ weight: "400", subsets: ['latin']});
